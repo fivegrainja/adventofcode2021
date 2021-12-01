2dadc5fbc944e389d28636f9093a437f7fb3b810
Jason Stevens <jason@fivegrain.com>
2021-12-01 18:35:52 -0500

    Single py for each day. aoc_utils improvements.

---
+++ day01/day01.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+import aoc_utils
+
+day = '01'
+test_assertion_a = 7
+test_assertion_b = 5
+
+def part_a(lines):
+    depths = [int(line) for line in lines if line != '']
+    count = 0
+    for i in range(len(depths)-1):
+        if depths[i] < depths[i+1]:
+            count += 1
+    return count
+
+def part_b(lines):
+    depths = [int(line) for line in lines if line != '']
+    count = 0
+    for i in range(len(depths)-3):
+        if sum(depths[i:i+3]) < sum(depths[i+1:i+4]):
+            count += 1
+    return count
+
+aoc_utils.test_and_execute(part_a, day, test_assertion_a)
+aoc_utils.test_and_execute(part_b, day, test_assertion_b)
+
---
+++ template/day00.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+import aoc_utils
+
+day = '00'
+test_assertion_a = None
+test_assertion_b = None
+
+def part_a(lines):
+    # nums = [int(l) for l in lines if l != '']
+    return
+
+def part_b(lines):
+    # nums = [int(l) for l in lines if l != '']
+    return
+
+aoc_utils.test_and_execute(part_a, day, test_assertion_a)
+aoc_utils.test_and_execute(part_b, day, test_assertion_b)
