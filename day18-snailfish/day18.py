ce2ef2fd4177ea0df11b17f873f3d6b70431b30a
Jason Stevens <jason@fivegrain.com>
2021-12-20 09:17:38 -0500

    Some cleanup of day 18

--- day18-snailfish/day18.py
+++ day18-snailfish/day18.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# See day<number>-questions.txt for context to this solution
+# See day18-questions.txt for context to this solution
 
 import sys
 from rich import print
@@ -15,8 +15,17 @@ test_assertion_a = None
 test_assertion_b = None
 
 class Node:
+    """ Represents a node in a binary tree. Each of these nodes represents a snailfish number.
+    That is, it is either a regular number (an integer value) or a pair of other snailfish
+    numbers (its child nodes.) Note that a node has exactly one of the following:
+    a. A value (in which case it has no child nodes)
+    b. Two child nodes (in which case in has no value)
+    """
 
     def __init__(self, snailfish_number=None):
+        """ The optional snailfish_number is a string representing a snailfish number
+        to initialize this node as.
+        """
         self.left = None
         self.right = None
         self.value = None
@@ -29,21 +38,34 @@ class Node:
                 self.value = int(snailfish_number)
     
     def reduce(self):
+        """ Reduce this snailfish number by exploding or splitting itself or subnodes as needed.
+        """
         while True:
-            # print(f'Reduce {self}')
-            # print('looping in reduce')
+            # Explode any numbers that should be exploded.
             if self.explode():
+                # A node exploded, so reiterate
                 continue
-            # print('Got past the explode')
+            # Split any number that should be split
             if self.split():
+                # A node split, so reiterate
                 continue
+            # We don't have any nodes that need to explode or split, so exit the loop
             break
     
     @staticmethod
     def split_pair(snailfish_number):
+        """ Take a string representing a non-regular snailfish number ([...,...]) and
+        return the left and right substrings as split by the comma. Note that left and
+        right substrings might themselves contain commas, so attention to bracket
+        pairs is required.
+        """
         assert(snailfish_number[0] == '[')
         assert(snailfish_number[-1] == ']')
         num_opens = 0
+        # Iterate through the string, skipping the opening square bracket.
+        # Count open/close brackets. When we get to a comma that isn't enclosed
+        # by square brackets (not counting that opening one) then we've found
+        # the outtermost comma.
         for i in range(1, len(snailfish_number)):
             c = snailfish_number[i]
             if c == ',' and num_opens == 0:
@@ -55,21 +77,31 @@ class Node:
         raise Exception(f'Did not find split for {snailfish_number=}')
 
     def __repr__(self):
+        """ Return the string representation of this snailfish number
+        """
         if self.is_regular():
             return str(self.value)
         return f'[{self.left},{self.right}]'
     
     def __add__(self, other):
-        # print(f'__add__ {self=} {other=}')
+        """ Note that this only adds Nodes and does not account for string representations of
+        snailfish numbers being directly added.
+        """
         sum = Node(f'[{self},{other}]')
         sum.reduce()
         return sum
     
     def __eq__(self, other):
+        """ Equality. Note that this does not account for comparision with string representations
+        of snailfish numbers.
+        """
         # Arguably we should be comparing reduced versions of self and other
         return str(self) == str(other)
 
     def is_regular(self):
+        """ Return True if this snailfish number is a regular value (an integer), False otherwise.
+        """
+        # Adding some assertions just for error checking
         if self.value is None:
             assert(self.left is not None)
             assert(self.right is not None)
@@ -88,54 +120,54 @@ class Node:
         # exploding pair (if any). Exploding pairs will always consist of two
         # regular numbers. Then, the entire exploding pair is replaced with the
         # regular number 0.
+
+        # Strategy: Perform a Depth First Search (left to right) for the first node that is more
+        # than 4 levels deep. Explode that node. Note that exploding requires changes to the 
+        # closest regular values to the left and right of the exploded node. We track the latest
+        # regular value as we search. Once we explode we continue the DFS (without exploding an
+        # additional nodes) to find the next regular value to the right.
         queue = [(self, 1)]
-        last_regular_node = None
-        explode_right_value = None
-        any_changes = False
+        last_regular_node = None # The closest regular node to the left
+        explode_right_value = None # The value to add to the closest node to the right
+        any_changes = False # Return value - True if we made any changes, False otherwise.
         while queue:
             node, level = queue.pop()
-            # print(f'Popped {node} at level {level}')
-            # if len(queue) > 10:
-            #     sys.exit()
-            # If this is a regular number then save it for use in the explode operation
             if node.is_regular():
-                # print(f'Found regular number {node.value}')
                 if explode_right_value is not None:
-                    # print(f'assigning it the value for the right')
+                    # We already exploded a node, now we have the next regular value to the right.
+                    # Do the addition and break out of the loop, we're done.
                     node.value += explode_right_value
-                    # Already set any_changes when we exploded
                     break
-                # print(f'recording it for last reg left')
+                # Keep track of the latest regular node from the left for use
+                # when we explode a node.
                 last_regular_node = node
             elif explode_right_value is None and level > 4 and node.left.is_regular() and node.right.is_regular():
-                # print(f'Found a pair to explode {node.left.value},{node.right.value}')
                 # Time to explode
                 if last_regular_node is not None:
-                    # print(f'{last_regular_node=}')
+                    # We have a regular node to the left
                     last_regular_node.value += node.left.value
-                    last_regular_node = None # Not necessary, just tidy
-                explode_right_value = node.right.value
-                # print(f'{explode_right_value=}')
+                    # last_regular_node = None # Not necessary, just tidy
+                explode_right_value = node.right.value # For later use
                 node.left = None
                 node.right = None
                 node.value = 0
                 any_changes = True
             else:
                 # Push both children onto the queue, right then left (so left will be prioritiezed)
-                # print(f'Pushing {node.left} and {node.right}')
                 queue.append((node.right, level + 1))
                 queue.append((node.left, level + 1))
-            # sys.exit()
         return any_changes
     
     def split(self):
+        """ Perform a DFS (left to right) looking for a regular node with value >= 10. Split the first
+        one found. Return True if a node was split, False otherwise.
+        """
         queue = [self]
         while queue:
             node = queue.pop()
             if node.is_regular():
                 if node.value >= 10:
                     # Split this node
-                    # print(f'Splitting {node.value}')
                     left = node.value // 2
                     right = node.value - left
                     node.left = Node(str(left))
@@ -148,119 +180,113 @@ class Node:
         return False
     
     def magnitude(self):
+        """ Return the magnitude of this snailfish number
+        """
         if self.is_regular():
             return self.value
         return 3 * self.left.magnitude() + 2 * self.right.magnitude()
         
-def part_a(lines):
-    # nums = [int(l) for l in lines if l != '']
-    return
+# What follows is many of the examples from the question as tests for the code. Uncomment
+# the entire block to enable tests.
 
-def part_b(lines):
-    # nums = [int(l) for l in lines if l != '']
-    return
-
-
-
-
-for input, expected in [
+# for input, expected in [
+# # ("""[1,1]
+# # [2,2]
+# # [3,3]
+# # [4,4]""".split(),
+# # '[[[[1,1],[2,2]],[3,3]],[4,4]]'),
 # ("""[1,1]
 # [2,2]
 # [3,3]
-# [4,4]""".split(),
-# '[[[[1,1],[2,2]],[3,3]],[4,4]]'),
-("""[1,1]
-[2,2]
-[3,3]
-[4,4]
-[5,5]""".split(),
-'[[[[3,0],[5,3]],[4,4]],[5,5]]')
-]:
-    nodes = [Node(line.strip()) for line in input if line.strip()]
-    # print(nodes)
-    sum = functools.reduce(lambda a,b:a+b, nodes)
-    # print(f'{sum=} {expected=}')
-    assert(str(sum) == str(expected))
+# [4,4]
+# [5,5]""".split(),
+# '[[[[3,0],[5,3]],[4,4]],[5,5]]')
+# ]:
+#     nodes = [Node(line.strip()) for line in input if line.strip()]
+#     # print(nodes)
+#     sum = functools.reduce(lambda a,b:a+b, nodes)
+#     # print(f'{sum=} {expected=}')
+#     assert(str(sum) == str(expected))
 
 
-for input, output in (
-    ('[[[[[9,8],1],2],3],4]', '[[[[0,9],2],3],4]'),
-    ('[7,[6,[5,[4,[3,2]]]]]', '[7,[6,[5,[7,0]]]]'),
-    ('[[6,[5,[4,[3,2]]]],1]', '[[6,[5,[7,0]]],3]'),
-    ('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]', '[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'),
-    ('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]', '[[3,[2,[8,0]]],[9,[5,[7,0]]]]')
-):
-    # print()
-    # print(f'Testing {input=} {output=}')
-    node = Node(input)
-    node.explode()
-    # print(f'Result {input=} {output=} {str(node)=}')
-    assert(str(node) == output)
+# for input, output in (
+#     ('[[[[[9,8],1],2],3],4]', '[[[[0,9],2],3],4]'),
+#     ('[7,[6,[5,[4,[3,2]]]]]', '[7,[6,[5,[7,0]]]]'),
+#     ('[[6,[5,[4,[3,2]]]],1]', '[[6,[5,[7,0]]],3]'),
+#     ('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]', '[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'),
+#     ('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]', '[[3,[2,[8,0]]],[9,[5,[7,0]]]]')
+# ):
+#     # print()
+#     # print(f'Testing {input=} {output=}')
+#     node = Node(input)
+#     node.explode()
+#     # print(f'Result {input=} {output=} {str(node)=}')
+#     assert(str(node) == output)
 
-for input, output in (
-    ('[10,8]', '[[5,5],8]'),
-    ('[4,[8,11]]', '[4,[8,[5,6]]]')
-):
-    # print()
-    # print(f'Testing {input=} {output=}')
-    node = Node(input)
-    node.split()
-    # print(f'Result {input=} {output=} {str(node)=}')
-    assert(str(node) == output)
+# for input, output in (
+#     ('[10,8]', '[[5,5],8]'),
+#     ('[4,[8,11]]', '[4,[8,[5,6]]]')
+# ):
+#     # print()
+#     # print(f'Testing {input=} {output=}')
+#     node = Node(input)
+#     node.split()
+#     # print(f'Result {input=} {output=} {str(node)=}')
+#     assert(str(node) == output)
 
-# print('summing')
-sum = Node('1') + Node('2') + Node('3')
-# print(f'{sum=}')
+# # print('summing')
+# sum = Node('1') + Node('2') + Node('3')
+# # print(f'{sum=}')
 
-sum = functools.reduce(lambda a, b: a + b, (Node('[1,1]'), Node('[2,2]'), Node('[3,3]'),Node('[4,4]')))
-# print(f'{sum=}')
-assert(str(sum) == '[[[[1,1],[2,2]],[3,3]],[4,4]]')
+# sum = functools.reduce(lambda a, b: a + b, (Node('[1,1]'), Node('[2,2]'), Node('[3,3]'),Node('[4,4]')))
+# # print(f'{sum=}')
+# assert(str(sum) == '[[[[1,1],[2,2]],[3,3]],[4,4]]')
 
-sum = Node('[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]') + Node('[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]')
-# print(sum)
-# print('[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]')
-
-
-input = [
-    '[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]',
-    '[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]',
-    '[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]',
-    '[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]',
-    '[7,[5,[[3,8],[1,4]]]]',
-    '[[2,[2,2]],[8,[8,1]]]',
-    '[2,9]',
-    '[1,[[[9,3],9],[[9,0],[0,7]]]]',
-    '[[[5,[7,4]],7],1]',
-    '[[[[4,2],2],6],[8,7]]']
-nodes = [Node(i) for i in input]
-sum = nodes[0]
-for other in nodes[1:]:
-    new_sum = sum + other
-    # print(f'{sum} + {other} = {new_sum}')
-    sum = new_sum
+# sum = Node('[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]') + Node('[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]')
+# # print(sum)
+# # print('[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]')
 
 
-# sum = functools.reduce(lambda a,b: a+b, nodes)
-assert(str(sum) == '[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]')
+# input = [
+#     '[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]',
+#     '[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]',
+#     '[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]',
+#     '[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]',
+#     '[7,[5,[[3,8],[1,4]]]]',
+#     '[[2,[2,2]],[8,[8,1]]]',
+#     '[2,9]',
+#     '[1,[[[9,3],9],[[9,0],[0,7]]]]',
+#     '[[[5,[7,4]],7],1]',
+#     '[[[[4,2],2],6],[8,7]]']
+# nodes = [Node(i) for i in input]
+# sum = nodes[0]
+# for other in nodes[1:]:
+#     new_sum = sum + other
+#     # print(f'{sum} + {other} = {new_sum}')
+#     sum = new_sum
 
 
-input = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-[[[5,[2,8]],4],[5,[[9,9],0]]]
-[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-[[[[5,4],[7,7]],8],[[8,3],8]]
-[[9,3],[[9,9],[6,[4,9]]]]
-[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]""".split()
-nodes = [Node(line.strip()) for line in input if line.strip()]
-sum = functools.reduce(lambda a,b:a+b, nodes)
-expected = '[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]'
-# print(f'{sum=} {expected=}')
-assert(str(sum) == str(expected))
-# print(f'{sum.magnitude()=}')
-assert(sum.magnitude() == 4140)
+# # sum = functools.reduce(lambda a,b: a+b, nodes)
+# assert(str(sum) == '[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]')
+
+
+# input = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+# [[[5,[2,8]],4],[5,[[9,9],0]]]
+# [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+# [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+# [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+# [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+# [[[[5,4],[7,7]],8],[[8,3],8]]
+# [[9,3],[[9,9],[6,[4,9]]]]
+# [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+# [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]""".split()
+# nodes = [Node(line.strip()) for line in input if line.strip()]
+# sum = functools.reduce(lambda a,b:a+b, nodes)
+# expected = '[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]'
+# # print(f'{sum=} {expected=}')
+# assert(str(sum) == str(expected))
+# # print(f'{sum.magnitude()=}')
+# assert(sum.magnitude() == 4140)
 
 print('PART A')
 path = Path(__file__).parent / 'day18-input.txt'
@@ -278,11 +304,4 @@ for a, b in itertools.permutations(nodes, 2):
     max_magnitude = max(max_magnitude, mag)
 print(f'Max magnitude is {max_magnitude}')
 
-
 print('DONE')
-
-
-# test_and_execute is a utility function that runs the function with the correct input, checks that the 
-# output for the test data is the expected value, prints time it took to run, etc.
-# aoc_utils.test_and_execute(part_a, day, test_assertion_a, Path(__file__).parent)
-# aoc_utils.test_and_execute(part_b, day, test_assertion_b, Path(__file__).parent)
---
+++ day19-beacon-scanner/day19.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+# See day<number>-questions.txt for context to this solution
+
+import sys
+from rich import print
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+import aoc_utils
+
+# import collections # https://docs.python.org/3/library/collections.html
+# import numpy # https://numpy.org/doc/stable/user/quickstart.html
+# import pandas # https://pandas.pydata.org/docs/getting_started/index.html#getting-started
+# import itertools # https://docs.python.org/3/library/itertools.html
+# import more_itertools # https://github.com/more-itertools/more-itertools
+# import operator # https://docs.python.org/3/library/operator.html#module-operator
+
+# import networkx # https://networkx.org/documentation/stable/tutorial.html
+# import networkx as nx
+# import matplotlib.pyplot as plt
+# g1 = nx.petersen_graph()
+# nx.draw(g1)
+# plt.show()
+
+
+day = '19'
+test_assertion_a = None
+test_assertion_b = None
+
+def part_a(lines):
+    # nums = [int(l) for l in lines if l != '']
+    return
+
+def part_b(lines):
+    # nums = [int(l) for l in lines if l != '']
+    return
+
+# test_and_execute is a utility function that runs the function with the correct input, checks that the 
+# output for the test data is the expected value, prints time it took to run, etc.
+aoc_utils.test_and_execute(part_a, day, test_assertion_a, Path(__file__).parent)
+aoc_utils.test_and_execute(part_b, day, test_assertion_b, Path(__file__).parent)
